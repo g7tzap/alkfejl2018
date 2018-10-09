7b64c5c503e0a6c495cd954ad4ad0e1a6425a24c
g7tzap <43700023+g7tzap@users.noreply.github.com>
2018-10-09 13:15:13 +0200

    Add files via upload

---
+++ src/main/java/hu/receptblog/alkfejlesztes/g7tzap/fejer/Receptblog/ReceptblogApplication.java
@@ -0,0 +1,12 @@
+package hu.receptblog.alkfejlesztes.g7tzap.fejer.Receptblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptblogApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/receptblog/alkfejlesztes/g7tzap/fejer/Receptblog/ReceptblogApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.receptblog.alkfejlesztes.g7tzap.fejer.Receptblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReceptblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
