c5fe551b2111b5614e4488b53078eb949aa95c57
g7tzap <43700023+g7tzap@users.noreply.github.com>
2018-10-10 21:17:24 +0200

    Add files via upload
    A security működik, de a cmd-ből kell kimásolni a jelszót, nem emlékszem, h hogy kell neki tetszőlegeset állítani.

---
+++ src/main/java/hu/receptblog/alkfejlesztes/g7tzap/fejer/Receptblog/model/Comment.java
@@ -0,0 +1,39 @@
+package hu.receptblog.alkfejlesztes.g7tzap.fejer.Receptblog.model;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import hu.receptblog.alkfejlesztes.g7tzap.fejer.Receptblog.model.Recipe;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Column
+    private String author;
+
+    @Column
+    private String text;
+
+    @ManyToOne
+    @JoinColumn
+    @JsonIgnore
+    private Recipe recipe;
+}
