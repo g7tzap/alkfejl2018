766587c4b206cad30f14f0bd228746922016d239
Tamás Fejér <ftomi89@gmail.com>
2018-12-11 15:24:00 +0100

    Fixed CrossOrigin

--- src/main/java/hu/alkfejlesztes/g7tzap/fejer/receptblog/controller/RecipeController.java
+++ src/main/java/hu/alkfejlesztes/g7tzap/fejer/receptblog/controller/RecipeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,7 @@ class RecipeController {
     private RecipeService recipeService;
 
     @GetMapping("/all")
+    @CrossOrigin
     public List<Recipe> all() {
         return recipeService.findAll();
     }
